04becac9c3610219c42f2dae2da6590099872eb0
Ian Breno Oliveira Silva <Ianbos218@gmail.com>
2026-05-14 20:16:48 -0300

    Fix: use_env_variable in models/index.js for Neon production DB

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -7,7 +7,7 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    url: process.env.DATABASE_URL,
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     dialectOptions: {
       ssl: {
